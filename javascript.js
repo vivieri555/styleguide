2056d5fe017c49cc845241c558786680c7f78b2b
unknown <vivieri555@gmail.com>
2026-05-05 14:57:03 +0200

    changes in loading

--- javascript.js
+++ javascript.js
@@ -9,3 +9,15 @@ function showPage() {
     document.getElementById("loadingText").style.display = "none";
     document.getElementById("isLoading").style.display = "block";
 }
+
+const changeText = document.getElementById('loadingBtn');
+changeText.addEventListener('click', function() {
+    var pic = document.getElementById('loadCode');
+    if (pic.style.display === 'none' || pic.style.display === '') {
+        pic.style.display = 'block';
+        this.textContent = 'Dölj kod';
+    } else {
+        pic.style.display = 'none';
+        this.textContent = 'Klicka här för kodexempel';
+    }
+});
