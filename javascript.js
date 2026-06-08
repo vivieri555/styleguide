3607b84799e74e91a8e4d3e06de83f3b0a921aa8
unknown <vivieri555@gmail.com>
2026-06-08 12:41:33 +0200

    hide menu options

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+hideAllSections();
+
 /* laddningsindikator relaterat */
 var myVar;
 
@@ -22,11 +24,79 @@ changeText.addEventListener('click', function() {
         this.textContent = 'Klicka här för kodexempel';
     }
 });
+//        Länkar för att visa/dölja menyval
+function hideAllSections() {
+  const sections = [
+"headlines", "fluentText", "divBtns", "tableContainer", "navigation", "msgWarn",
+"loadHead", "panel", "formsContainer", "colorContainer"
+  ];
+  sections.forEach(id => {
+    const section = document.getElementById(id);
+    if(section) section.classList.add("hidden");
+  });
+}
+function hideEachSection(id, div) {
+  if (id) {
+  id.addEventListener("click", function(e) {
+    e.preventDefault();
+    hideAllSections();
+    const targetDiv = document.getElementById(div);
+      if (targetDiv) {
+      targetDiv.classList.remove("hidden");
+      }
+    });
+  }
+}
+const headlinesLink = document.getElementById("headlinesLink");
+if (headlinesLink) {
+    hideEachSection(headlinesLink, "headlines");
+}
+const fluentTextLink = document.getElementById("fluentTextLink");
+if(fluentTextLink) {
+  hideEachSection(fluentTextLink, "fluentText");
+}
+
+const divBtnsLink = document.getElementById("divBtnsLink");
+if(divBtnsLink) {
+  hideEachSection(divBtnsLink, "divBtns");
+}
+
+const tableContainerLink = document.getElementById("tableContainerLink");
+if(tableContainerLink) {
+  hideEachSection(tableContainerLink, "tableContainer");
+}
+const navigationLink = document.getElementById("navigationLink");
+if(navigationLink) {
+  hideEachSection(navigationLink, "navigation");
+}
+
+const msgWarnLink = document.getElementById("msgWarnLink");
+if(msgWarnLink) {
+  hideEachSection(msgWarnLink, "msgWarn");
+}
+const loadHeadLink = document.getElementById("loadHeadLink");
+if(loadHeadLink) {
+  hideEachSection(loadHeadLink, "loadHead");
+}
+const panelLink = document.getElementById("panelLink");
+if(panelLink) {
+  hideEachSection(panelLink, "panel");
+}
+
+const formsContainerLink = document.getElementById("formsContainerLink");
+if(formsContainerLink) {
+  hideEachSection(formsContainerLink, "formsContainer");
+}
+const colorContainerLink = document.getElementById("colorContainerLink");
+if(colorContainerLink) {
+  hideEachSection(colorContainerLink, "colorContainer");
+}
+
 /* Drop down menyn*/
 function myFunction2() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-/* toggla mellan att visa o ta bort menyn visuellt */
+/* visa meny */
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
